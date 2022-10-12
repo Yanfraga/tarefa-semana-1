f28d232177f966eefcc775a42b83e9e56500ef05
YanFraga <yan.oliveira.fraga@gmail.com>
2022-10-11 21:47:42 -0300

    adicionar email na pratica 5 e tabuada na pratica 6

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,13 @@
+let nome
+let email
+let profissao
+let idade
+let lugar
+
+nome="Yan Fraga"
+email="yan.oliveira.fraga@gmail.com"
+profissao="engenheiro"
+idade=27
+lugar="Dubai"
+
+console.log("Olá, " + nome+ "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para" + lugar +". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é" + profissao + "e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
