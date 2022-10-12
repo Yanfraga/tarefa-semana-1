bc20c10cbaa0e9abe0dbc2f809b0d4a7a4ccfa05
YanFraga <yan.oliveira.fraga@gmail.com>
2022-10-11 21:04:17 -0300

    adicionar mostrar true ou false na pratica 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,23 @@
+let nome
+let email
+let profissao
+let idade
+let lugar
+
+nome="Yan Fraga"
+email="yan.oliveira.fraga@gmail.com"
+profissao="engenheiro"
+idade=27
+lugar="Dubai"
+
+const oNome = typeof(nome)== "string"
+const oEmail = typeof(email)== "string"
+const aProfissao = typeof(profissao)=="string"
+const aIdade = typeof(idade)=="string"
+const oLugar = typeof(lugar)=="string"
+
+console.log(oNome)
+console.log(oEmail)
+console.log(aProfissao)
+console.log(aIdade)
+console.log(oLugar)
