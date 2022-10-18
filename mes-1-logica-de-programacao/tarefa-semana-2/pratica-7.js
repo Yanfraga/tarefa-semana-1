f623fcd05b61d649b08077c6ac0fa628e6f15665
YanFraga <yan.oliveira.fraga@gmail.com>
2022-10-18 14:50:21 -0300

    adicionando pratica 7

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -0,0 +1,14 @@
+
+function tradutor (idioma){
+    if(idioma=="en"){
+        console.log("Hello World!")
+    }if(idioma =="es"){
+        console.log("Hola mundo!")
+    }if(idioma =="pt"){
+        console.log("Olá mundo!")
+    }
+else{
+    console.log("Olá mundo!")
+}
+}
+tradutor("es")
