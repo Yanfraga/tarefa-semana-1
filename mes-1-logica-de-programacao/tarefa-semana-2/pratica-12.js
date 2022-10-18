f0509636c8d06da49fee3f59bebbd2ba067a0fa0
YanFraga <yan.oliveira.fraga@gmail.com>
2022-10-18 16:45:32 -0300

    adicionando pratica 12

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-12.js
@@ -0,0 +1,27 @@
+const dia = '9'
+switch(dia){
+    case '1':
+        console.log('segunda')
+        break
+    case '2':
+        console.log('terça')
+        break 
+     case '3':
+        console.log('quarta')
+        break 
+     case '4':
+        console.log('quinta')
+        break 
+    case '5':
+        console.log('sexta')
+        break
+    case '6':
+        console.log('sábado')
+        break
+    case '7':
+        console.log('domingo')
+        break
+    default:
+        console.log(`O número ${dia} não é dia da semana.`)         
+
+}
